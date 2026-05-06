c197b66763a7fb0b6f1bdb7c92eb275adf3618eb
Abhiram <abhiramjoshi0707@gmail.com>
2026-05-06 11:33:59 +0530

    changed angular.json

--- parkSmart-frontend-main/parksmart-ng/src/app/interceptor/base-url.interceptor.ts
+++ parkSmart-frontend-main/parksmart-ng/src/app/interceptor/base-url.interceptor.ts
@@ -6,7 +6,7 @@ import {
   HttpRequest
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environments';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class BaseUrlInterceptor implements HttpInterceptor {
