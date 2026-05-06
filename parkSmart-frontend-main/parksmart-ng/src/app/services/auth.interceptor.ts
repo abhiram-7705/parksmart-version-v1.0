32e16a71b5354fba2dfd848079c23cce0c97a175
abhiram-7705 <147041608+abhiram-7705@users.noreply.github.com>
2026-05-06 12:26:46 +0530

    Delete parkSmart-frontend-main/parksmart-ng directory

--- parkSmart-frontend-main/parksmart-ng/src/app/services/auth.interceptor.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(req.clone({ withCredentials: true }));
-  }
-}
--- parkSmart-frontend-main/parksmart-ng/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://parksmart-version-v10-production.up.railway.app'
-};
